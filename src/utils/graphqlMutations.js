3a5dd7ad039d78dbacaadb77b1add48c6a47ef14
ShogunRoss <nguyenconglong95@gmail.com>
2019-10-04 00:57:23 +0700

    *Update info

--- src/utils/graphqlMutations.js
+++ src/utils/graphqlMutations.js
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag';
 
+// Used for signing in with email and password
+// Get back accessToken when success
 const SIGN_IN = gql`
   mutation SignIn($email: String!, $password: String!) {
     signIn(email: $email, password: $password) {
@@ -8,44 +10,48 @@ const SIGN_IN = gql`
   }
 `;
 
+// Used for signing up with email and password
+// Get back true when success
 const SIGN_UP = gql`
   mutation SignUp($email: String!, $password: String!) {
     signUp(email: $email, password: $password)
   }
 `;
 
+// This one is deprecated - The /confirm endpoint is now doing this job.
+// Can consider to using this in the future
 const CONFIRM_EMAIL = gql`
   mutation ConfirmEmail($confirmToken: String!) {
     confirmEmail(confirmToken: $confirmToken)
   }
 `;
 
-const CREATE_EVENT = gql`
-  mutation CreateEvent($deviceId: ID!) {
-    createEvent(deviceId: $deviceId) {
-      action
-    }
-  }
-`;
-
+//Used for sending an forgot password url to recipient's email
+//Get back true when success
 const SEND_FORGOT_PASSWORD_EMAIL = gql`
   mutation SendForgotPasswordEmail($email: String!) {
     sendForgotPasswordEmail(email: $email)
   }
 `;
 
+//Used for change password when signed in
+//Get back true when success
 const CHANGE_PASSWORD = gql`
   mutation ChangePassword($newPassword: String!) {
     changePassword(newPassword: $newPassword)
   }
 `;
 
+//Used for reset password within forgot password url
+//Get back true when success
 const RESET_PASSWORD = gql`
   mutation ResetPassword($newPassword: String!, $passwordToken: String!) {
     resetPassword(newPassword: $newPassword, passwordToken: $passwordToken)
   }
 `;
 
+//Used for signing out
+//Get back true when success
 const SIGN_OUT = gql`
   mutation SignOut {
     signOut
@@ -56,7 +62,6 @@ export {
   SIGN_IN,
   SIGN_UP,
   CONFIRM_EMAIL,
-  CREATE_EVENT,
   SEND_FORGOT_PASSWORD_EMAIL,
   CHANGE_PASSWORD,
   RESET_PASSWORD,
--- src/utils/graphqlQueries.js
+++ src/utils/graphqlQueries.js
@@ -1,7 +1,9 @@
 import gql from 'graphql-tag';
 
+// Used for query information of user who signed in
+// Can get back all User field - However, if you don't any field, you should not mention it
 const ME = gql`
-  {
+  query {
     me {
       email
       id
@@ -9,6 +11,8 @@ const ME = gql`
   }
 `;
 
+// Used for query information of device which has an <id> as input
+// Can get back all Device field - However, if you don't any field, you should not mention it
 const DEVICE_STATE = gql`
   query Device($id: ID!) {
     device(id: $id) {
