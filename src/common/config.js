2a1f4c6eda8594da5f4d3cc87f28e0eb2e392655
Long Nguyen <nguyenconglong95@gmail.com>
2022-01-03 21:53:09 +0700

    chore: update

--- src/common/config.js
+++ src/common/config.js
@@ -1,2 +1,2 @@
 // export const URL = 'http://171.244.143.79:8000';
-export const URL = 'https://hospital-management-2022.herokuapp.com/';
+export const URL = 'https://hospital-management-2022.herokuapp.com';
