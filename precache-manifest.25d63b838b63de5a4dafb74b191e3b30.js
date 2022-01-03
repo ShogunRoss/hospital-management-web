6a9232e98dbace318b3fcf8eaece48814aa2a26e
Long Nguyen <nguyenconglong95@gmail.com>
2022-01-03 21:03:51 +0700

    Updates

---
+++ precache-manifest.25d63b838b63de5a4dafb74b191e3b30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f713924bfdcb63b310bf96facbd9d875",
+    "url": "/hospital-management-web/index.html"
+  },
+  {
+    "revision": "fa27124235d60ebf25a7",
+    "url": "/hospital-management-web/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "dd7d3dba902f9ec984c9",
+    "url": "/hospital-management-web/static/css/main.1d65d520.chunk.css"
+  },
+  {
+    "revision": "fa27124235d60ebf25a7",
+    "url": "/hospital-management-web/static/js/2.a63d53d6.chunk.js"
+  },
+  {
+    "revision": "9f2609038a01d637b099e88d3406990d",
+    "url": "/hospital-management-web/static/js/2.a63d53d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7d3dba902f9ec984c9",
+    "url": "/hospital-management-web/static/js/main.e1181d6b.chunk.js"
+  },
+  {
+    "revision": "eb26f313ce233030937a",
+    "url": "/hospital-management-web/static/js/runtime-main.c0c76f2b.js"
+  }
+]);
